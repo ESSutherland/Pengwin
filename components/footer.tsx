6ee5d784f26f9bab0545deb8e39a15c449844c62
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-29 15:01:47 -0500

    updated styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,10 +3,11 @@ import { CgVercel } from "react-icons/cg";
 
 const Footer = () => {
   return (
-    <footer className="hidden sm:block">
+    <footer className="hidden sm:block z-10">
       <p className="text-xs flex items-center justify-center text-sky-300">
         Pengwin &copy; 2024, All rights reserved. | Powered by{" "}
-        <CgVercel className="w-5 h-5" /> Vercel
+        <CgVercel className="w-5 h-5" />
+        Vercel
       </p>
     </footer>
   );
