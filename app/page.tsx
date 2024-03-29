5a8591acd815fd55c51278c8707f91fd76ea40bf
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-29 14:55:10 -0500

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-"use client";
-
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import Snow from "@/components/snow";
 import StreamPreview from "@/components/stream-preview";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import { Snowfall } from "react-snowfall";
-import { useMedia } from "react-use";
 
 export default function Home() {
-  const is3XL = useMedia("(min-width: 2000px)", false);
-
   return (
     <div className="font-simplestamp h-full w-full text-sky-100 bg-gradient-to-tr from-sky-400 to-sky-500 relative">
-      <Snowfall snowflakeCount={is3XL ? 300 : 150} />
+      <Snow />
       <div className="bg-[url('/bottom-corner.png')] h-[400px] w-[400px] bg-contain absolute bottom-0 bg-no-repeat" />
       <div className="bg-[url('/top-corner.png')] h-[300px] w-[300px] bg-contain absolute top-0 right-0 bg-no-repeat" />
+
       <div className="w-full h-full max-w-[1140px] mx-auto flex flex-col items-center">
         <Navbar />
         <p className="text-center tracking-widest blue-shadow-lg uppercase text-[3rem] sm:hidden block">
@@ -34,6 +31,7 @@ export default function Home() {
             </Link>
           </Button>
         </div>
+        <Footer />
       </div>
     </div>
   );
---
+++ components/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { CgVercel } from "react-icons/cg";
+
+const Footer = () => {
+  return (
+    <footer className="hidden sm:block">
+      <p className="text-xs flex items-center justify-center text-sky-300">
+        Pengwin &copy; 2024, All rights reserved. | Powered by{" "}
+        <CgVercel className="w-5 h-5" /> Vercel
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
