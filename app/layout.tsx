d3930694cff409fdf7759c6b0183bd0e4b7cdbb2
Ethan Sutherland <ESutherland95@gmail.com>
2024-03-29 04:09:53 -0500

    fixed typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Penwin",
+  title: "Pengwin",
   description:
     "Pengwin is a twitch streamer who plays games and does other stuff.",
 };
